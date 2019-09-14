560fe6762a74049f728688e26049780a230d1039
PadmaPrasad <padmaprasad91@gmail.com>
2019-09-14 19:42:38 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/com/myapp/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.myapp.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
